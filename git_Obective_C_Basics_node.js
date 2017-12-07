e22d31b30edc2da60b7745c11638ab98a950372c
Captain <onepiece.captain97@gmail.com>
2017-12-07 22:47:25 +0800

    update in Thu Dec 07 2017 22:47:25 GMT+0800 (CST)

---
+++ git_Obective_C_Basics_node.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+let child_process = require('child_process');
+let path = require('path')
+
+
+
+console.log(path.resolve(__dirname, 'Obective_C_Basics'))
+
+
+ let iocmds = [
+    /*
+    `rm -rf /home/cloud/temp/`,
+    `rm -rf /home/cloud/hadoop-2.6.4/dfs/data/current/`,
+    `rm -rf /home/cloud/hadoop-2.6.4/dfs/name/current/`,
+    */
+]
+
+let cmds = [
+    `git add .`,
+    `git commit -m 'update in ${new Date()}'`,
+    `git push`
+]
+
+
+
+for (let iocmd of iocmds) {
+    child_process.execSync(iocmd, (err, stdout, stderr) => {
+        if (err) return console.log(err)
+        console.log(stdout)
+    });
+}
+
+
+for (let cmd of cmds) {
+    child_process.execSync(cmd, {
+        cwd: '/Users/Captain/Downloads/Git_Rrpositories/Obective_C_Basics'
+    }, (err, stdout, stderr) => {
+        if (err) return console.log(err)
+        console.log(stdout)
+    });
+}
