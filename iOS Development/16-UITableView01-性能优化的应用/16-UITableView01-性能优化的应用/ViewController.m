f99265317d96ce4fc332808837b03db4372de5e3
Captain <onepiece.captain97@gmail.com>
2018-02-18 23:19:11 +0800

    update in Sun Feb 18 2018 23:19:11 GMT+0800 (CST)

--- iOS Development/16-UITableView01-性能优化的应用/16-UITableView01-性能优化的应用/ViewController.m
+++ iOS Development/16-UITableView01-性能优化的应用/16-UITableView01-性能优化的应用/ViewController.m
@@ -17,7 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 1.加载plist，array中装的都是NSDictionary对象
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"shop.plist" ofType:nil];
+    //NSString *path = [[NSBundle mainBundle] pathForResource:@"shop" ofType:@"plist"];
+    
+    NSArray *array = [NSArray arrayWithContentsOfFile:path];    // arrayWithContentsOfFile:方法 传得是 全路径
+    
 }
+
+#pragma mark - 数据源方法
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 15;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 1. 定义一个标识
+    NSString *ID = [NSString stringWithFormat:@"cell-TB"];
+    
+    // 2.去缓存池里取出可循环利用的cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];   // 注意：dequeueReusableCellWithIdentifier:是tableView的类方法
+    
+    // 3. 若缓存中没有可循环利用的cell
+    if ( cell == nil) {
+         cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleDefault) reuseIdentifier:ID];
+    }
+    
+    return cell;
+}
+
 
 
 - (void)didReceiveMemoryWarning {
